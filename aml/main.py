e5f3ee1ad0d3c35276b8c2841a5c4b57a8b3304a
Jakub Janicek <97741078+VeolCZ@users.noreply.github.com>
2025-04-25 11:19:04 +0200

    Dataset label parsing (#2)

--- aml/main.py
+++ aml/main.py
@@ -1,20 +1,19 @@
 import sys
-
-from hello_world import hello
+from make_labels import make_labels
 
 
 def help_func() -> None:
     print("Available commands:")
     for arg, (_, desc) in command_map.items():
         print(f"  {arg}: {desc}")
-    print("\nYou can combine multiple commands which) ", end="")
-    print("will be executed in order, e.g.: --hello --goodbye")
+    print("\nYou can combine multiple commands which will be executed in order, e.g.: --parse --goodbye")
 
 
 command_map = {
-    '--hello': (hello, "Hello world"),
+    '--make_labels': (make_labels, "Creates a labels.csv file in /data"),
     '--help': (help_func, "Shows this help message"),
 }
+
 
 def main() -> None:
     if len(sys.argv) < 2 or all(arg not in command_map for arg in sys.argv[1:]):
@@ -29,6 +28,7 @@ def main() -> None:
                 executed_commands.add(arg)
             elif arg not in command_map:
                 print(f"Warning: Unknown command '{arg}'")
+
 
 if __name__ == "__main__":
     main()
