e976524e68f0fd0d0c51f8f77e2afbd351b69445
Kania <kaniakozikowska@gmail.com>
2025-05-08 01:26:42 +0200

    hopefully working tree baseline

--- aml/random_forests/random_forest_classifier.py
+++ aml/random_forests/random_forest_classifier.py
@@ -3,44 +3,52 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
 from numpy.typing import NDArray
-from aml.preprocessing.TreeImageDataset import Label
-from aml.preprocessing.TreeImageDataset import TreeImageDataset
+from preprocessing.TreeImageDataset import TreeImageDataset
 from torch.utils.data import DataLoader
 import torch
 
 
 def train_random_forest(
-    x: NDArray[np.float64], y: list[Label], test_size: float = 0.2
+    x: NDArray[np.float64], y, test_size: float = 0.2
 ) -> tuple[RandomForestClassifier, float]:
     forest_classifier = RandomForestClassifier()
     x_train, x_test, y_train, y_test = train_test_split(
         x, y, test_size=test_size, stratify=y
     )
+    print("Fitting has begun... dun dun dunnn...")
     forest_classifier.fit(x_train, y_train)
+    print("predicting, hopefully better than my future")
     y_pred = forest_classifier.predict(x_test)
+    print("figuring out if this thing is better than random guessing")
     accuracy = accuracy_score(y_test, y_pred)
     print("Accuracy: ", accuracy)
     return forest_classifier, accuracy
 
-def my_collate_fn(data):
-    images, labels = zip(*data)
-    label_ids = [label.label for label in labels]
-    return images, label_ids
+def my_collate_fn(batch):
+    images, labels = zip(*batch)
+    return list(images), list(labels)
 
-if __name__ == "__main__":
-    train_dataloader = DataLoader(TreeImageDataset("train"))
+
+
+def tree_test():
+    train_dataloader = DataLoader(TreeImageDataset("train"), collate_fn=my_collate_fn)
     all_x = []
     all_y = []
-    print("Loading data...")
-    print(type(train_dataloader))
     idx = 0
-    
-    to_pass = [44, 45, 46]
 
     for x_batch, y_batch in train_dataloader:
         print(f"Batch {idx}")
         idx += 1
-        all_x.append(x_batch)
-        all_y.append(y_batch)
+        if idx >= 5000:
+            print("Karolina's computer likes being alive")
+            break
 
+        x_batch_np = np.array([x for x in x_batch], dtype=np.float64)
+        y_batch_np = [int(label["cls"].view(-1)[0].item()) for label in y_batch]
+        all_x.append(x_batch_np)
+        all_y.extend(y_batch_np)
+
+    all_x = np.vstack(all_x)
+    all_y = np.array(all_y, dtype=np.int64)
     train_random_forest(all_x, all_y)
+
--- aml/random_forests/random_forest_regressor.py
+++ aml/random_forests/random_forest_regressor.py
@@ -1,1 +1,50 @@
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
+from numpy.typing import NDArray
+from preprocessing.TreeImageDataset import TreeImageDataset
+from torch.utils.data import DataLoader
+import torch
+
+
+def train_random_forest(
+    x: NDArray[np.float64], y, test_size: float = 0.2
+) -> tuple[RandomForestRegressor, float]:
+    forest_regressor = RandomForestRegressor()
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=test_size, stratify=y
+    )
+    forest_regressor.fit(x_train, y_train)
+    y_pred = forest_regressor.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy: ", accuracy)
+    return forest_regressor, accuracy
+
+def my_collate_fn(data):
+    images, labels = zip(*data)
+    label_ids = [label.label for label in labels]
+    return images, label_ids
+
+
+def treereg_test():
+    train_dataloader = DataLoader(TreeImageDataset("train"), collate_fn=my_collate_fn)
+    all_x = []
+    all_y = []
+    idx = 0
+
+    for x_batch, y_batch in train_dataloader:
+        print(f"Batch {idx}")
+        idx += 1
+        if idx >= 5000:
+            print("Karolina's computer likes being alive")
+            break
+
+        x_batch_np = np.array([x for x in x_batch], dtype=np.float64)
+        y_batch_np = [int(label["cls"].view(-1)[0].item()) for label in y_batch]
+        all_x.append(x_batch_np)
+        all_y.extend(y_batch_np)
+
+    all_x = np.vstack(all_x)
+    all_y = np.array(all_y, dtype=np.int64)
+    train_random_forest(all_x, all_y)
