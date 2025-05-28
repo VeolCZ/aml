5a052a52879511f9348766af93a48ea825a98110
VeolCZ <jakub.gembler@gmail.com>
2025-05-28 15:58:30 +0200

    Merge remote-tracking branch 'origin/main' into dev

---
+++ aml/random_forests/forest_train_util.py
@@ -0,0 +1,62 @@
+import os
+import numpy as np
+from random_forests.RandomForestClassifier import RandomForestClassifierModel
+from random_forests.RandomForestRegressor import RandomForestRegressorModel
+from torch.utils.data import Subset
+from sklearn.model_selection import train_test_split
+from preprocessing.TreeImageDataset import TreeImageDataset
+
+
+SEED = int(os.getenv("SEED", 123))
+
+
+def train_classifier_forest() -> None:
+    """
+    Run the random forest training for classification.
+    """
+    PATH = "/logs/forest_classifier.pkl"
+    model = RandomForestClassifierModel()
+
+    train_dataset = TreeImageDataset(type="train")
+    eval_dataset = TreeImageDataset(type="eval")
+
+    all_labels = train_dataset.get_cls_labels()
+    train_indices, test_indices, _, _ = train_test_split(
+        np.arange(len(train_dataset)),
+        all_labels,
+        test_size=0.5,
+        stratify=all_labels,
+        random_state=SEED
+    )
+
+    train_dataset_subset = Subset(train_dataset, train_indices)
+    _test_dataset = Subset(eval_dataset, test_indices)
+
+    model.fit(train_dataset_subset)
+    model.save_model(PATH)
+
+
+def train_regressor_forest() -> None:
+    """
+    Run the random forest training for regression.
+    """
+    PATH = "/logs/forest_regressor.pkl"
+    model = RandomForestRegressorModel()
+
+    train_dataset = TreeImageDataset(type="train")
+    eval_dataset = TreeImageDataset(type="eval")
+
+    all_labels = train_dataset.get_cls_labels()
+    train_indices, test_indices, _, _ = train_test_split(
+        np.arange(len(train_dataset)),
+        all_labels,
+        test_size=0.5,
+        stratify=all_labels,
+        random_state=SEED
+    )
+
+    train_dataset_subset = Subset(train_dataset, train_indices)
+    _test_dataset = Subset(eval_dataset, test_indices)
+
+    model.fit(train_dataset_subset)
+    model.save_model(PATH)
