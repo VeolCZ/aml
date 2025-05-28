4bfe0619ec7128d3682cf668301fd8c0a2fe4935
Jakub Janicek <97741078+VeolCZ@users.noreply.github.com>
2025-05-28 10:10:42 +0200

    Adding safe guards and no GPU support (#18)

--- aml/random_forests/forest_train_util.py
+++ aml/random_forests/forest_train_util.py
@@ -1,86 +1,35 @@
 import os
 import numpy as np
 from random_forests.CompositeRandomForest import CompositeRandomForest
-from random_forests.RandomForestClassifier import RandomForestClassifierModel
-from random_forests.RandomForestRegressor import RandomForestRegressorModel
 from torch.utils.data import Subset
 from sklearn.model_selection import train_test_split
 from preprocessing.TreeImageDataset import TreeImageDataset
 
 
 SEED = int(os.getenv("SEED", 123))
-
-
-def train_classifier_forest() -> None:
-    """
-    Run the random forest training for classification.
-    """
-    PATH = "/logs/forest_classifier.pkl"
-    model = RandomForestClassifierModel()
-
-    train_dataset = TreeImageDataset(type="train")
-    eval_dataset = TreeImageDataset(type="eval")
-
-    all_labels = train_dataset.get_cls_labels()
-    train_indices, test_indices, _, _ = train_test_split(
-        np.arange(len(train_dataset)),
-        all_labels,
-        test_size=0.1,
-        stratify=all_labels,
-        random_state=SEED
-    )
-
-    train_dataset_subset = Subset(train_dataset, train_indices)
-    _test_dataset = Subset(eval_dataset, test_indices)
-
-    model.fit(train_dataset_subset)
-    model.save_model(PATH)
-
-
-def train_regressor_forest() -> None:
-    """
-    Run the random forest training for regression.
-    """
-    PATH = "/logs/forest_regressor.pkl"
-    model = RandomForestRegressorModel()
-
-    train_dataset = TreeImageDataset(type="train")
-    eval_dataset = TreeImageDataset(type="eval")
-
-    all_labels = train_dataset.get_cls_labels()
-    train_indices, test_indices, _, _ = train_test_split(
-        np.arange(len(train_dataset)),
-        all_labels,
-        test_size=0.1,
-        stratify=all_labels,
-        random_state=SEED
-    )
-
-    train_dataset_subset = Subset(train_dataset, train_indices)
-    _test_dataset = Subset(eval_dataset, test_indices)
-
-    model.fit(train_dataset_subset)
-    model.save_model(PATH)
+TEST_SIZE = 0.1
 
 
 def train_composite_forest() -> None:
+    assert os.path.exists("/data/CUB_200_2011"), "Please ensure the dataset is properly extracted into /data"
+    assert os.path.exists("/weights"), "Please ensure the /weights directory exists"
+    assert os.path.exists("/data/labels.csv"), "Please ensure the labels are generated (--make_labels)"
+
     PATH = "/weights/forest"
     model = CompositeRandomForest()
 
     train_dataset = TreeImageDataset(type="train")
-    eval_dataset = TreeImageDataset(type="eval")
 
     all_labels = train_dataset.get_cls_labels()
-    train_indices, test_indices, _, _ = train_test_split(
+    train_indices, _, _, _ = train_test_split(
         np.arange(len(train_dataset)),
         all_labels,
-        test_size=0.1,
+        test_size=TEST_SIZE,
         stratify=all_labels,
         random_state=SEED
     )
 
     train_dataset_subset = Subset(train_dataset, train_indices)
-    _test_dataset = Subset(eval_dataset, test_indices)
 
     model.fit(train_dataset_subset)
     model.save_model(PATH)
